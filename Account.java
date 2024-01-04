a620579833094f9ffe3445283eea0ee9d52b6308
Panavat0113 <141387589+Panavat0113@users.noreply.github.com>
2024-01-05 00:21:36 +0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,26 @@
+public class Account {
+    public double balance;
+    public String name;
+    public void deposit(double b){
+        if (b >= 0){
+            balance = balance + b;
+        }else{
+            System.out.println("The balance variable must be greater than or equal to zero.");
+        }
+    }
+    public double withdraw(double b){
+        if (balance-b < 0) {
+            System.out.println("Your account balance is insufficient.");
+            return 0;
+        }else if (b >= 0){
+            balance = balance - b;
+            return b;
+        }else{
+            System.out.println("The balance variable must be greater than or equal to zero.");
+            return 0;
+        }
+    }
+    public void showInfo(){
+        System.out.println("In "+name+" account, there is a balance equal to "+balance+" baht. ");
+    }
+}
