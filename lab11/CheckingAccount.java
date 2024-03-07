12e68fe6162ad6cac14318900cd0c7b7ac3af7e9
Panavat0113 <141387589+Panavat0113@users.noreply.github.com>
2024-03-07 17:07:19 +0700

    Add files via upload

---
+++ lab11/CheckingAccount.java
@@ -0,0 +1,40 @@
+public class CheckingAccount extends Account{
+    private double credit;
+    public CheckingAccount(){
+        this(0, "",0);
+    }
+    public CheckingAccount(double balance, String name, double credit){
+        super(balance, name);
+        this.credit = credit;
+    }
+    public void setCredit(double credit){
+        if (credit > 0){
+            this.credit = credit;
+        }else{
+            System.out.println("Input number must be a positive integer.");
+        }
+    }
+    public double getCredit(){
+        return credit;
+    }
+    @Override
+    public void withdraw(double a) throws WithdrawException{
+        if(balance - a >= 0){
+            balance -= a;
+            System.out.println(a +" baht is withdrawn from "+ name +" and your credit balance is "+ credit +".");
+        }else if ((balance - a + credit) >= 0){
+            credit -= balance-a;
+            balance = 0;
+            System.out.println(a +" baht is withdrawn from "+ name +" and your credit balance is "+ credit +".");
+        }else{
+            throw new WithdrawException("Account <"+this.getName()+"> has not enough money.");
+        }
+    }
+    public void withdraw(String a) throws WithdrawException{
+        this.withdraw(Double.parseDouble(a));
+    }
+    @Override
+    public String toString(){
+        return "The "+ name +" account has "+ balance+" baht and "+ credit +" credits.";
+    }
+}
---
+++ lab11/main.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String[] args) {
+    CheckingAccount acct1 = new CheckingAccount(1000, "A0001", 500);
+    Customer cust = new Customer("Sompong", "Sookjai", acct1);
+    try{
+        cust.getAcct().deposit(500);
+        cust.getAcct().withdraw(1800);
+        cust.getAcct().withdraw(300);
+        cust.getAcct().deposit(1000);
+        cust.getAcct().withdraw(200);
+    }
+    catch(WithdrawException e){
+        System.out.println(e.getMessage());
+    }
+    finally{
+            System.out.println("Thank you.");
+        }
+}
+}
